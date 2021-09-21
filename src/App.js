8c9f272a2a8e9eb48bd3a5aa8ce5d40dbf0311df
Dan Chen <deyingchen@yahoo.com>
2021-09-21 11:02:32 -0500

    Added more changes

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ export default function App() {
               <Link to="/login">Login</Link>
             </li>
             <li>
-              <Link to="/reset">Reset  MMMM</Link>
+              <Link to="/reset">Reset  MMMMore Changes</Link>
             </li>
             <li>
               <Link to="/dashboard">Dashboard</Link>
